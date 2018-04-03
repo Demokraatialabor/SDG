05340b6fd5cf3b98ca06d7d0192fb944ec8cf165
Keegan David Braun Mcbride <kemcbr@ttu.ee>
2018-04-03 16:44:28 +0300

    Initial functionality completed

--- Application/app.r
+++ Application/app.r
@@ -2,73 +2,110 @@ library(shiny)
 library(shinydashboard)
 library(tidyverse)
 library(readxl)
+library(DT)
+
 #loadData
 SDG_Data <- as.data.frame(read_excel("Data/Näitajad_töötuba.xlsx"))
-SDG_Data[, 2:13] <- sapply(SDG_Data[,2:13], as.numeric)
+rownames(SDG_Data) <- SDG_Data[,1]
+SDG_Data [,1] <- NULL
+SDG_Data[, 1:13] <- sapply(SDG_Data[,1:13], as.numeric)
 uhisData <- SDG_Data[1:4, ]
+rownames(uhisData)[1] <- "Total %" 
 polData <- SDG_Data[6:7, ]
 rm(SDG_Data)
+
+dataset_choices <- c("uhisData", "polData")
 
 ui <- dashboardPage(
   skin = "green",
   dashboardHeader(title = "SDG Vizualization Demo"),
   dashboardSidebar(sidebarMenu(
-    menuItem("Home", tabName = "home", icon = icon("dashboard")),
-    menuItem(
-      "Data Explorer",
-      tabName = "explore"
-    )
+    menuItem("Home", tabName = "home", icon = icon("dashboard"))
   )),
   dashboardBody(tabItems(
     tabItem("home",
             fluidRow(
-              box(
-                title = "Heas ja rahuldavas seisukorras olevate ehitismälestiste osakaal",
-                width = 4,
-                height = 100,
-                background = "green"
-              ),
-              box(
-                title = "Linnades toimunud liiklusõnnetuste tagajärjel vigastatute ja hukkunute arv",
-                width = 4,
-                height = 100,
-                background = "yellow"
-              ),
-              box(
-                title = "Peenosakesed PM10 ja PM2,5 välisõhus",
-                width = 4,
-                height = 100,
-                background = "red"
-              )
+              valueBoxOutput("seisukorras"),
+              valueBoxOutput("linnades"),
+              valueBoxOutput("peenosakesed")),
+            fluidRow(
+              valueBoxOutput("roheline"),
+              valueBoxOutput("rahulolu"),
+              valueBoxOutput("uhistransport")),
+            fluidRow(
+              selectInput("dataset", "Choose Dataset to Display and Download:", choices = c("Public Transport",
+                                                                                            "Pollution Data")),
+              DT::dataTableOutput("dataTable")
             ),
             fluidRow(
-              box(
-                title = "Rohealad linnades",
-                width = 4,
-                height = 100,
-                background = "green"
-              ),
-              box(
-                title = "Rahulolu eluruumide seisundiga",
-                width = 4,
-                height = 100,
-                background = "green"
-              ),
-              box(
-                title = "Ühistranspordiga, jalgrattaga või jalgsi tööl käijate osakaal",
-                width = 4,
-                height = 100,
-                background = "yellow"
-              )
-            )),
-    tabItem("explore",
-            fluidRow(box(title = "Data Exploration",
-                         plotOutput("plot1", height = 250))))
-  ))
-)
+              downloadButton("downloadData", "Download Data"),
+              plotOutput("dataplot")
+            )
+    ))))
+#   tabItem("explore",
+#            fluidRow( plotOutput("plotUhisTransport"))))
+
 
 server <- function(input, output) {
-
+  output$seisukorras <- renderValueBox({
+    valueBox("85%", "Heas ja rahuldavas seisukorras olevate ehitismälestiste osakaal
+", icon = icon("building") , color = "green"
+             
+    )  })
+  output$linnades <- renderValueBox({
+    valueBox("4.5", "Linnades toimunud liiklusõnnetuste tagajärjel vigastatute ja hukkunute arv", icon = icon("car") , color = "yellow"
+             
+    )  })
+  output$peenosakesed <- renderValueBox({
+    valueBox(paste0(polData[1,12], "% PM10"), "Peenosakesed PM10 ja PM2,5 välisõhus", icon = icon("industry") , color = "green"
+             
+    ) })
+  output$roheline <- renderValueBox({
+    valueBox("17%", "Rohealad linnades
+", icon = icon("leaf") , color = "red"
+             
+    ) })
+  output$rahulolu <- renderValueBox({
+    valueBox("80%","Rahulolu eluruumide seisundiga
+", icon = icon("smile-o") , color = "green"
+             
+    ) })
+  output$uhistransport <- renderValueBox({
+    valueBox(paste0(uhisData[1,13], "%"), "Ühistranspordiga, jalgrattaga või jalgsi tööl käijate osakaal", icon = icon("bus") , color = "yellow"
+    )
+  })
+  
+  datasetInput <- reactive({
+    switch(input$dataset,
+           
+           "Public Transport" = uhisData,
+           "Pollution Data" = polData
+    )
+  })
+  
+  output$dataTable <- DT::renderDataTable({
+    datasetInput()
+    
+  })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file, row.names= FALSE)
+    }
+  )
+  
+  output$dataplot <- renderPlot({
+    if(input$dataset == "Public Transport") {
+      plot(colnames(uhisData), uhisData[1,1:13], "l", xlab = "Year", ylab = "Ühistranspordiga tööl käijate osatähtsus hõivatute seas, %")
+    }
+    else{
+      plot(colnames(polData), polData[1,1:13], "l", xlab= "Year", ylab ="Peenosakeste PM10 sisaldus välisõhus, tuhat tonni", color = "blue")
+    }
+  })
+  
 }
 
 
