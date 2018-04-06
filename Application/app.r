0bdcf8305476843432d7e1f623f561164fbad30f
Keegan David Braun Mcbride <kemcbr@ttu.ee>
2018-04-06 11:59:40 +0300

    Added in SDG 12 and some minor UI changes.

--- Application/app.r
+++ Application/app.r
@@ -1,16 +1,14 @@
 library(shiny)
 library(shinydashboard)
-library(tidyverse)
 library(readxl)
 library(DT)
 
 #loadData
-SDG_Data <- as.data.frame(read_excel("Data/Näitajad_töötuba.xlsx"))
+SDG_Data <- as.data.frame(read_excel("Data/sdgdata.xlsx"))
 rownames(SDG_Data) <- SDG_Data[,1]
 SDG_Data [,1] <- NULL
 SDG_Data[, 1:13] <- sapply(SDG_Data[,1:13], as.numeric)
 uhisData <- SDG_Data[1:4, ]
-rownames(uhisData)[1] <- "Total %" 
 polData <- SDG_Data[6:7, ]
 rm(SDG_Data)
 
@@ -20,11 +18,13 @@ ui <- dashboardPage(
   skin = "green",
   dashboardHeader(title = "SDG Vizualization Demo"),
   dashboardSidebar(sidebarMenu(
-    menuItem("Home", tabName = "home", icon = icon("dashboard"))
+    menuItem("SDG11", tabName = "sdg11", icon = icon("building")),
+    menuItem("SDG12", tabName = "sdg12", icon = icon("trash"))
   )),
   dashboardBody(tabItems(
-    tabItem("home",
+    tabItem("sdg11",
             fluidRow(
+              tags$head(tags$style(HTML(".small-box {height: 150px}"))),
               valueBoxOutput("seisukorras"),
               valueBoxOutput("linnades"),
               valueBoxOutput("peenosakesed")),
@@ -33,23 +33,38 @@ ui <- dashboardPage(
               valueBoxOutput("rahulolu"),
               valueBoxOutput("uhistransport")),
             fluidRow(
-              selectInput("dataset", "Choose Dataset to Display and Download:", choices = c("Public Transport",
-                                                                                            "Pollution Data")),
+              column(3, selectInput("dataset", "Choose Dataset: ", choices = c("Public Transport",
+                                                                               "Pollution Data"))),
+              # column(3, selectInput("viz", "Choose a visualization:", choices = c("Bar Graph",
+              #                                                           "Line Graph",
+              #                                                           "Area Graph",
+              #                                                           "Pie Chart"
+              #                                                           ))),
               DT::dataTableOutput("dataTable")
             ),
             fluidRow(
               downloadButton("downloadData", "Download Data"),
+              HTML("<br><br>"),
               plotOutput("dataplot")
             )
-    ))))
-#   tabItem("explore",
-#            fluidRow( plotOutput("plotUhisTransport"))))
+    ),
+    tabItem("sdg12",
+            fluidRow(
+              box("something here")
+            ))
+  )
+  )
+  
+  
+)
+
+
 
 
 server <- function(input, output) {
   output$seisukorras <- renderValueBox({
     valueBox("85%", "Heas ja rahuldavas seisukorras olevate ehitismälestiste osakaal
-", icon = icon("building") , color = "green"
+             ", icon = icon("building") , color = "green"
              
     )  })
   output$linnades <- renderValueBox({
@@ -62,12 +77,12 @@ server <- function(input, output) {
     ) })
   output$roheline <- renderValueBox({
     valueBox("17%", "Rohealad linnades
-", icon = icon("leaf") , color = "red"
+             ", icon = icon("leaf") , color = "red"
              
     ) })
   output$rahulolu <- renderValueBox({
     valueBox("80%","Rahulolu eluruumide seisundiga
-", icon = icon("smile-o") , color = "green"
+             ", icon = icon("smile-o") , color = "green"
              
     ) })
   output$uhistransport <- renderValueBox({
@@ -99,10 +114,10 @@ server <- function(input, output) {
   
   output$dataplot <- renderPlot({
     if(input$dataset == "Public Transport") {
-      plot(colnames(uhisData), uhisData[1,1:13], "l", xlab = "Year", ylab = "Ühistranspordiga tööl käijate osatähtsus hõivatute seas, %")
+      plot(colnames(uhisData), uhisData[1,1:13], "b", xlab = "Year", ylab = "Ühistranspordiga tööl käijate osatähtsus hõivatute seas, %")
     }
     else{
-      plot(colnames(polData), polData[1,1:13], "l", xlab= "Year", ylab ="Peenosakeste PM10 sisaldus välisõhus, tuhat tonni", color = "blue")
+      plot(colnames(polData), polData[1,1:13], "b", xlab= "Year", ylab ="Peenosakeste PM10 sisaldus välisõhus, tuhat tonni", color = "blue")
     }
   })
   
